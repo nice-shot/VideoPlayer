b94699c2557e405456dfa178cb2d55d943664ddf
nice_shot <shefferu@gmail.com>
2018-07-18 09:44:06 +0300

    Added icons and started integrating them

--- src/video_controls.js
+++ src/video_controls.js
@@ -4,15 +4,21 @@ const luxon = require("luxon");
 // Setup html elements
 const videoElem = document.getElementsByTagName("video")[0];
 const playPauseBtn = document.getElementById("playPause");
+const playPauseIcon = playPauseBtn.getElementsByTagName("img")[0];
 const currentTimeElem = document.getElementsByClassName("currentTime")[0];
 
+// Play-pause images
+const playImg = "images/play.png"
+const pauseImg = "images/pause.png"
 
 // Utility functions
 function PlayPause() {
     if (videoElem.paused) {
         videoElem.play();
+        playPauseIcon.src = pauseImg;
     } else {
         videoElem.pause();
+        playPauseIcon.src = playImg;
     }
 }
 
